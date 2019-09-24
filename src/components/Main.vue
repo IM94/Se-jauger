9d07d344c933fb4bd326850bc6000c632618db56
unknown <paulinevolpi@hotmail.com>
2019-09-24 15:12:46 +0200

    ajout server.js; delete helloWorld

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <h1>Se Jauger</h1>
+    </v-row>
+    <v-row justify="center">
+      <p>Jauge ta personalité en répondant à ces question</p>
+    </v-row>
+    <v-row justify="center">
+      <v-btn>S'identifier</v-btn>
+    </v-row>
+    <v-row justify="center">
+      <v-btn>Pas encore inscrit? Clique ici!</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    // data et méthodes
+  })
+}
+</script>
