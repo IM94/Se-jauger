ede6a4abd3c9b92f44c9c2cdad82b330a6aaa440
IM94 <48949070+IM94@users.noreply.github.com>
2019-10-29 23:17:10 +0100

    app principale

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
   <v-app>
-  <v-content>
-    <Main/>
-  </v-content>
+    <Menu v-on:menu_item="menu_item($event)" v-on:connected="connected($event)"/>
+    <Accueil  v-if="item === `Accueil`"/>
+    <Quizz    v-if="item === `Quizz`"/>
+    <Admin v-if="connect && item === `Admin`"/>
+    <Bottom />
   </v-app>
 </template>
 
 <script>
-import Main from './components/Main'
-
+import Menu from './component/Monmenu.vue'
+import Accueil from './component/Accueil.vue'
+import Quizz from './component/Quizz.vue'
+import Bottom from './component/Bottom.vue'
+import Admin from './component/Admin.vue'
 export default {
   name: 'App',
   components: {
-    Main
+    Menu,
+    Accueil,
+    Quizz,
+    Bottom,
+    Admin
   },
   data: () => ({
-    //
-  })
+    item: `Accueil`,
+    connect: false
+  }),
+  methods: {
+    menu_item: function (e) {
+      this.item = e
+    },
+    connected: function (e) {
+      this.connect = e
+    }
+  },
+  created () {
+  }
 }
 </script>
+
+<style scoped>
+</style>
